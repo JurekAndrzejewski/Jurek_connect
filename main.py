cdb310ac22ed1a0a7e7038c50a44a0229723d2b3
JurekAndrzejewski <88549154+JurekAndrzejewski@users.noreply.github.com>
2022-08-25 15:54:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import os
 import get_bl_orders
 import update_orders
-
+def prepare_workspace():
+    if(not os.path.exists('logs')):
+        os.mkdir('logs')
+    if(not os.path.exists('csv_files')):
+        os.remove('csv_files')
+        
 def main():
     get_bl_orders.execute()
     update_orders.execute()
